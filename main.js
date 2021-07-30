f34c8b931afa2dbc4f98be13f3bd14d86fb6bd13
Mike Ball <mike.ball3@gmail.com>
2021-07-29 22:29:11 -0400

    more ideas. just a sketch of what I'm thinking ATM....subject to change as we learn more

--- main.js
+++ main.js
@@ -36,9 +36,74 @@ function chooseARandomAction(currentPageElements){
  */
 async function runAction(page, action){
   //TODO: MB to implement
-  
+
 }
 
+
+/**
+ *  
+ * @param {Object} browser playwright browser object (assuming we need it?)
+ * @param {Object} context playwright context object (assumcing we need it)
+ * @param {Object} page playwricht page object
+ * * @returns null if false or true if there is an error
+ */
+async function checkForErrors(browser, context, page){
+  //TODO: Implement
+}
+
+/**
+ * returns if the strings are different
+ * obviously this implementation is overly simplified.
+ * We might need to make it more complex
+ * 
+ * @param {String} previousPage previous url
+ * @param {String} currentPage current url
+ */
+function didPageChange(previousPage, currentPage){
+  return previousPage !== currentPage;
+}
+
+
+/**
+ * prints the log and exits the program
+ * @param {Array} actionLog list of actions take by automation
+ */
+function printActionLogAndExit(actionLog){
+  //TODO: Implement
+}
+
+
+/**
+ * 
+ * this represents an intial idea of how this might work
+ * we can change this as we develop more
+ * 
+ * 
+ * @param {Object} browser playwright browser object
+ * @param {Object} context playwright context object
+ * @param {Object} page playwright page object
+ * @param {Array} actionLog history of actions
+ */
+async function runLoop(browser, context, page, actionLog){
+
+  let elements = await findAllRelaventElements(page);
+
+  let randomAction = chooseARandomAction(elements);
+
+  actionLog.push(randomAction);
+
+  await runAction(page, randomAction);
+
+  let errors = await checkForErrors(browser, context, page);
+
+  if(!errors){
+    //Recursive call!
+    await runLoop(browser, context, page, actionLog)
+  }
+  else{
+    printActionLogAndExit(actionLog);
+  }
+}
 
 
 
@@ -49,8 +114,10 @@ async function main(){
   });
   const context = await browser.newContext();
   const page = await context.newPage();
-  //TODO: Build the main loop
+  await login(page);
 
+  //TODO: Build the main loop
+  await runLoop(browser, context, page, []);
 }
 
 
